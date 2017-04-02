7504f8c72d615f9d97a2d05aab5c5f4ddc559815
JasonHo <hexing_h@hotmail.com>
2017-04-02 13:44:10 -0400

    Back up.

--- Test.java
+++ Test.java
@@ -1,26 +1,26 @@
 import java.util.ArrayList;
 public class Test {
 	public static void main(String[] args) throws Exception {
-        readfile file = new readfile();
-        buildtree b = new buildtree();
+        ReadFile file = new ReadFile();
+        BuildTree b = new BuildTree();
 		ArrayList<Integer> freqtable = file.buildFreTable(args[0]);
 	    long start, end, costtime;
-	 //    //pairing heap
-	 //    start = System.currentTimeMillis();
-		// for (int i = 0; i < 1; i++) {
-	 //    	b.buildtreep(freqtable);
-	 //    }
-	 //    end = System.currentTimeMillis();
-	 //    costtime = end - start;
-	 //    System.out.println("Time cost of pairing Heap is " + costtime);
-	 //    //4-way heap
-		// start = System.currentTimeMillis();
-	 //    for (int i = 0; i < 1; i++) {
-	 //    	b.buildtreef(freqtable);
-	 //    }
-		// end = System.currentTimeMillis();
-	 //    costtime = end - start;
-	 //    System.out.println("Time cost of 4-way Heap is " + costtime);
+	   //pairing heap
+	    start = System.currentTimeMillis();
+		for (int i = 0; i < 10; i++) {
+	    	b.buildtreep(freqtable);
+	    }
+	    end = System.currentTimeMillis();
+	    costtime = end - start;
+	    System.out.println("Time cost of pairing Heap is " + costtime);
+	    //4-way heap
+		start = System.currentTimeMillis();
+	    for (int i = 0; i < 10; i++) {
+	    	b.buildtreef(freqtable);
+	    }
+		end = System.currentTimeMillis();
+	    costtime = end - start;
+	    System.out.println("Time cost of 4-way Heap is " + costtime);
 
 	    
 	    //binary heap
